66f13a02114d8b0efbbd3db0da82a58bdeb518a6
y-paradox <yadavakshat586@gmail.com>
2023-02-23 03:08:39 +0530

    changes made

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(false);
+    DeluxePizza dp = new DeluxePizza(true);
     System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
